132e56c500980a5dc0fb5cf6ac48edb019531e17
github username <itsDario1@gmail.com>
2019-02-12 20:36:40 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
 def reformat_languages(languages)
   # your code here
+  new_hash = Hash.new([])
+  languages.each do |type1,langPair|
+    langPair.each do |lang,type2|
+      if new_hash[lang].is_a?(Hash)
+        new_hash[lang][:style] << type1
+        # binding.pry
+      else
+        new_hash[lang] = {:type => type2.values[0], :style => [type1]}
+      end
+    end
+  end
+  new_hash
 end
