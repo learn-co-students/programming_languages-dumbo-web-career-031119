c2a2873c0542b5654666542a51607a1abea5a26b
tpr1992 <terence.pataneronan@gmail.com>
2019-03-10 22:25:26 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
   # your code here
+
+  new_hash = {}
+  languages.collect do |language_style, language_data|
+    language_data.collect do |key, val|
+      new_hash[key] = val
+      new_hash[key][:style] = []
+      new_hash[key][:style] << language_style
+    end
+  end
+  new_hash[:javascript][:style] << :oo
+  new_hash
 end
